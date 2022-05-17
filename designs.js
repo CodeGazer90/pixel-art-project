58d8bdd27fe1665288ebed84ace667a34e5765cd
Kenelle Moore <benevolentkstudios@gmail.com>
2022-05-17 15:31:47 -0400

    working on table

--- designs.js
+++ designs.js
@@ -3,8 +3,27 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+let grid = document.getElementById("sizePicker");
+let gridHeight = document.getElementById('inputHeight.value');
+let gridWidth = document.getElementById('inputWidth.value');
+let colorPicker = document.getElementById('colorPicker');
+let pickedColor = document.getElementById('colorPicker.value')
+let table = document.getElementById('pixelCanvas');
+let submit = document.getElementById('submitBtn');
+let tr = document.createElement('tr');
+let td = document.createElement('td');
 
+
+
+
+function makeGrid() {
 // Your code goes here!
+for (let i = 0; i <= gridHeight; i++){
+    table.appendChild("tr");
+    for(let j = 0; i <= gridWidth; i++){
+        table.appendChild('td');
+    }
+}
+
 
 }
