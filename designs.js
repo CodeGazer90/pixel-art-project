3e10139076a3c290325a33f836c29aa31f2db7ba
Kenelle Moore <benevolentkstudios@gmail.com>
2022-05-17 17:39:49 -0400

    The grid works!

--- designs.js
+++ designs.js
@@ -4,25 +4,24 @@
 // When size is submitted by the user, call makeGrid()
 
 let form = document.getElementById("sizePicker");
-let gridHeight = document.getElementById('inputHeight.value');
-let gridWidth = document.getElementById('inputWidth.value');
 let colorPicker = document.getElementById('colorPicker');
 let pickedColor = document.getElementById('colorPicker.value')
 let table = document.getElementById('pixelCanvas');
-let tr = document.createElement('tr');
-let td = document.createElement('td');
-
-
 
 
 function makeGrid(event) {
 // Your code goes here!
 console.log("function is working");
+let gridHeight = document.getElementById('inputHeight').value;
+let gridWidth = document.getElementById('inputWidth').value;
 
 for (let i = 0; i <= gridHeight; i++){
-    table = table.appendChild("tr");
-    for(let j = 0; i <= gridWidth; i++){
-        table.appendChild('td');
+    let tr = document.createElement('tr');
+    table.appendChild(tr);
+
+    for(let j = 0; j <= gridWidth; j++){
+        let td = document.createElement('td');
+        tr.appendChild(td);
     }
 }
 
